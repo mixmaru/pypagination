741e1703f5cd68935234e97dec95dcd2133394ce
maru <mix.touch777@gmail.com>
2019-01-05 14:59:38 +0900

    初期化例外追加

--- pagination.py
+++ pagination.py
@@ -1,5 +1,23 @@
 class Pagination():
     def __init__(self, total, per_page, current):
+        self._check_init_params(total, per_page, current)
         self._total = total
         self._per_page = per_page
         self._current = current
+
+    def get_offset(self):
+        return self._per_page * (self._current - 1)
+
+    def _check_init_params(self, total, per_page, current):
+        if total < 0 or per_page < 0 or current < 0:
+            raise self.InitException("totalが適正ではありません。total:{0} per_page:{1} current:{2}".format(total, per_page, current))
+
+        if per_page > total:
+            raise self.InitException("per_pageが適正ではありません。total:{0} per_page:{1} current:{2}".format(total, per_page, current))
+
+        first_of_current_page = (current - 1) * per_page
+        if not (0 <= first_of_current_page < total):
+            raise self.InitException("currentが適正ではありません。total:{0} per_page:{1} current:{2}".format(total, per_page, current))
+
+    class InitException(Exception):
+        pass
--- test_pagination.py
+++ test_pagination.py
@@ -3,6 +3,28 @@ from pagination import Pagination
 
 
 class TestPagination(unittest.TestCase):
-    def test_pagination_init(self):
+    def test_init(self):
         pagination = Pagination(total=100, per_page=10, current=1)
         self.assertIsNotNone(pagination)
+
+        with self.assertRaises(Pagination.InitException):
+            pagination = Pagination(total=-1, per_page=100, current=1)
+
+        with self.assertRaises(Pagination.InitException):
+            pagination = Pagination(total=100, per_page=101, current=1)
+
+        with self.assertRaises(Pagination.InitException):
+            pagination = Pagination(total=100, per_page=10, current=11)
+
+        with self.assertRaises(Pagination.InitException):
+            pagination = Pagination(total=100, per_page=-1, current=1)
+
+        with self.assertRaises(Pagination.InitException):
+            pagination = Pagination(total=100, per_page=10, current=-1)
+
+    def test_get_offset(self):
+        p1 = Pagination(total=100, per_page=10, current=1)
+        self.assertEqual(0, p1.get_offset())
+
+        p2 = Pagination(total=100, per_page=10, current=2)
+        self.assertEqual(10, p2.get_offset())
